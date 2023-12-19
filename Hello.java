e2e0948988702ebd677959175a3bd138d3bc87e2
Sumit <sumit.varshney@oodles.io>
2023-12-19 11:52:14 +0530

    First java program

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ class Hello
     public static void main(String...s)
     {
        System.out.println("Hello Java");
-        System.out.println("Hello sumit");
-    }
+        System.out.println("Hello Sumit");
+         System.out.println("Hello Spring Boot ");    }
   }
